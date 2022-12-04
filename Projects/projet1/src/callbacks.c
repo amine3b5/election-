54a68bdf42c2b965b108248b375aece15ae0ecb1
gramiaziz <115464868+gramiaziz@users.noreply.github.com>
2022-12-04 01:37:39 +0100

    Add files via upload
    hedhe update l callbacks.c

--- Projects/projet1/src/callbacks.c
+++ Projects/projet1/src/callbacks.c
@@ -8,6 +8,7 @@
 #include "interface.h"
 #include "support.h"
 int x;
+int y;
 
 
 void
@@ -156,7 +157,68 @@ void
 on_button_modi_gma_clicked             (GtkButton       *button,
                                         gpointer         user_data)
 {
+  GtkWidget *output ;
+  int id;
+  liste nouv;
+  gchar idl[50];
+  gchar idt[50];
+  gchar idm1[50];
+  gchar idm2[50];
+  gchar idm3[50];
+  gchar ch[50]="liste modifier avec succée!!!";
+  gchar sh[50]="veuillez verifié le ID de la liste!!!";
+  //GtkWidget *label = lookup_widget(GTK_WIDGET(button), "label39");
+  output=lookup_widget(button, "label39");
+  GtkWidget *entry = lookup_widget(GTK_WIDGET(button), "entry_idl_gma");
+  strcpy(idl,gtk_entry_get_text(GTK_ENTRY(entry)) );
+  GtkWidget *entry1 = lookup_widget(GTK_WIDGET(button), "entry_idt_gma");
+  strcpy(idt,gtk_entry_get_text(GTK_ENTRY(entry1)) );
+  GtkWidget *entry2 = lookup_widget(GTK_WIDGET(button), "entry_m1_gma");
+  strcpy(idm1,gtk_entry_get_text(GTK_ENTRY(entry2)) );
+  GtkWidget *entry3 = lookup_widget(GTK_WIDGET(button), "entry_m2_gma");
+  strcpy(idm2,gtk_entry_get_text(GTK_ENTRY(entry3)) );
+  GtkWidget *entry4 = lookup_widget(GTK_WIDGET(button), "entry_m3_gma");
+  strcpy(idm3,gtk_entry_get_text(GTK_ENTRY(entry4)) );
   
+id=atoi(idl);
+ if(y==1)
+{strcpy(nouv.ori,"gauche");}
+else if(y==2)
+{strcpy(nouv.ori,"centre");}
+else if(y==3)
+{strcpy(nouv.ori,"droite");}
+
+    
+    
+    liste anc;
+    FILE *f=fopen("listes_electorales.txt","r");
+    FILE *f2=fopen("nouv.txt","w");
+    if(f!=NULL && f2!=NULL)
+    {
+        while(fscanf(f,"%d %d %d %d %d %s %d\n",&anc.id,&anc.idtete,&anc.idm[0],&anc.idm[1],&anc.idm[2],anc.ori,&anc.nv)!=EOF)
+        {
+            if(anc.id!=id)
+            {   fprintf(f2,"%d %d %d %d %d %s %d\n",anc.id,anc.idtete,anc.idm[0],anc.idm[1],anc.idm[2],anc.ori,anc.nv);
+		//gtk_label_set_text( GTK_LABEL(output),sh);
+}
+                
+            else{
+		nouv.id=id;
+		nouv.idtete=atoi(idt);
+		nouv.idm[0]=atoi(idm1);
+		nouv.idm[1]=atoi(idm2);
+		nouv.idm[2]=atoi(idm3);
+		nouv.nv=0;
+                fprintf(f2,"%d %d %d %d %d %s %d\n",nouv.id,nouv.idtete,nouv.idm[0],nouv.idm[1],nouv.idm[2],nouv.ori,nouv.nv);
+            
+		gtk_label_set_text( GTK_LABEL(output),ch);}
+
+        }
+    }
+    fclose(f);
+    fclose(f2);
+    remove("listes_electorales.txt");
+    rename("nouv.txt","listes_electorales.txt");
 
 }
 
@@ -165,7 +227,45 @@ void
 on_button_supp_gma_clicked             (GtkButton       *button,
                                         gpointer         user_data)
 {
+  GtkWidget *output ;
+  int id;
+  liste nouv;
+  gchar idl[50];
+  gchar idt[50];
+  gchar ch[50];
+  gchar sh[50];
 
+  GtkWidget *entry = lookup_widget(GTK_WIDGET(button), "entry_idlsupp_gma");
+  strcpy(idl,gtk_entry_get_text(GTK_ENTRY(entry)) );
+  GtkWidget *entry1 = lookup_widget(GTK_WIDGET(button), "entry_idtsupp_gma");
+  strcpy(idt,gtk_entry_get_text(GTK_ENTRY(entry1)) );
+  output=lookup_widget(button, "label40");
+
+  id=atoi(idl);
+int supprimer_liste(char *fn, int id)
+{
+    int tr=0;
+    liste l;
+    FILE *f=fopen(fn,"r");
+    FILE *f2=fopen("nouv.txt","w");
+    if(f!=NULL && f2!=NULL)
+    {	
+	while(fscanf(f,"%d %d %d %d %d %s\n",&l.id,&l.idtete,&l.idm[0],&l.idm[1],&l.idm[2],l.ori)!=EOF)
+        {
+            if(l.id==id)
+		tr=1;
+                
+            else
+                fprintf(f2,"%d %d %d %d %d %s\n",l.id,l.idtete,l.idm[0],l.idm[1],l.idm[2],l.ori);
+        }
+    }
+    fclose(f);
+    fclose(f2);
+    remove(fn);
+    rename("nouv.txt",fn);
+    return tr;
+}
+  supprimer_liste("listes_electorales.txt",id);
 }
 
 
@@ -226,5 +326,32 @@ on_radiobutton3_dr_toggled             (GtkToggleButton *togglebutton,
 {
 if (gtk_toggle_button_get_active(GTK_RADIO_BUTTON (togglebutton)))
 {x=3;}
+}
+
+
+void
+on_radiobutton_mg_gma_toggled          (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+if (gtk_toggle_button_get_active(GTK_RADIO_BUTTON (togglebutton)))
+{y=1;}
+}
+
+
+void
+on_radiobutton_mc_gma_toggled          (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+if (gtk_toggle_button_get_active(GTK_RADIO_BUTTON (togglebutton)))
+{y=2;}
+}
+
+
+void
+on_radiobutton_md_gma_toggled          (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+if (gtk_toggle_button_get_active(GTK_RADIO_BUTTON (togglebutton)))
+{y=3;}
 }
 
